8153a5252c01df8ca90b59fc7cf6b796ef7eba05
TheoDalit <theodalit@gmail.com>
2021-03-06 12:45:59 -0800

    Initial commit

---
+++ zipcodes.py
@@ -0,0 +1,20 @@
+import pandas
+from sqlalchemy import create_engine
+
+# Imports the zipcodes csv into the database
+hostname="127.0.0.1"
+uname="root"
+pwd=""
+dbname="zipcodes"
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+            .format(host=hostname, db=dbname, user=uname, pw=pwd))
+
+tables = pandas.read_csv(r"C:\Users\hombo\PycharmProjects\CNA335_zipcodes\zip_code_database.csv")
+
+
+
+connection=engine.connect()
+tables.to_sql('zipcodes',con = engine, if_exists = 'replace')
+connection.close()
+print(tables)
